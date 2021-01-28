12bd991280092689b446efd6cad151c6510f6cf2
Ian <DrilonIan@yahoo.com>
2021-01-29 01:55:04 +0800

    creating login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { Message, Key } from "../icons";
 import "./login.scss";
 
 const index: React.FC = () => {
-  return <section className="card">LoginCard</section>;
+  return (
+    <div className="login-container">
+      <div className="circle"></div>
+      <div className="card">
+        <h1>Sign In</h1>
+        <div className="input">
+          <div>
+            <span>
+              <Message />
+            </span>
+            <input type="text" placeholder="Enter your Email" />
+          </div>
+          <div>
+            <span>
+              <Key />
+            </span>
+            <input type="text" placeholder="Enter your Password" />
+          </div>
+        </div>
+        <span>Forgot Password?</span>
+        <button>Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default index;
